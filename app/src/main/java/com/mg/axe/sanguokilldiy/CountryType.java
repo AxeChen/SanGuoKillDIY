87bdba7b8390f9e6a2e20c22bd37562f92e8ae57
axeChen <1137366723@qq.com>
2017-12-13 17:18:31 +0800

    项目创建
    Signed-off-by: axeChen <1137366723@qq.com>

---
+++ app/src/main/java/com/mg/axe/sanguokilldiy/CountryType.java
@@ -0,0 +1,34 @@
+package com.mg.axe.sanguokilldiy;
+
+/**
+ * Created by Zaifeng on 2017/12/13.
+ * 卡牌的种类
+ */
+
+public class CountryType {
+    /**
+     * 魏
+     */
+    public static final String WEI = "wei";
+    /**
+     * 蜀
+     */
+    public static final String SHU = "shu";
+    /**
+     * 吴
+     */
+    public static final String WU = "wu";
+    /**
+     * 群
+     */
+    public static final String QUN = "qun";
+    /**
+     * 神
+     */
+    public static final String GOD = "god";
+
+    /**
+     * 自定义卡片
+     */
+    public static final String CUSTOM = "custom";
+}
