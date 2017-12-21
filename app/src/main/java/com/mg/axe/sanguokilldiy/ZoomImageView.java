e401b373b83ed2077372fd2a1bb9dceac35d2ab0
axechen <1137366723@qq.com>
2017-12-22 01:24:24 +0800

    资源文件准备
    Signed-off-by: axechen <1137366723@qq.com>

--- app/src/main/java/com/mg/axe/sanguokilldiy/ZoomImageView.java
+++ app/src/main/java/com/mg/axe/sanguokilldiy/ZoomImageView.java
@@ -8,15 +8,18 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
-import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewConfiguration;
 import android.view.ViewTreeObserver;
 
+import java.lang.ref.WeakReference;
+
 /**
  * Created by AxeChen on 2017/12/19.
+ * OnGlobalLayoutListener：布局完全绘制完成之后会调用，可以用来获取View的真实高度。
+ * ScaleGestureDetector：用来处理多点触控。
  */
 
 public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
@@ -24,28 +27,80 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
         ScaleGestureDetector.OnScaleGestureListener,
         View.OnTouchListener {
 
-    private boolean init = false;
+    /**
+     * 初始化完毕
+     */
+    private boolean isInit = false;
 
+    /**
+     * 初始的缩放值
+     */
     private float mInitScale;
 
+    /**
+     * 中等缩放值
+     */
     private float mMidScale;
 
+    /**
+     * 最大的缩放值
+     */
     private float mMaxScale;
 
-    private Matrix matrix;
+    /**
+     * 图片缩放平移使用的matrix
+     */
+    private Matrix mMatrix;
 
     /**
      * 捕获用户的多点触控的缩放比例
      */
     private ScaleGestureDetector mScaleGestureDetector;
 
+    /**
+     * 获取系统决定的最小移动距离
+     */
     private int mTouchSlop;
+
+    /**
+     * 计算移动的距离是否可以移动
+     */
     private boolean isCanMove;
+
+    /**
+     * 是否可以左右移动。当图片到达边缘时便无法左右滑动
+     */
     private boolean isCheckLeftAndRight;
+
+    /**
+     * 是否可以左右移动。当图片到达边缘时便无法上下滑动
+     */
     private boolean isCheckTopAndBottom;
 
-    // 双击放大缩小
+    /**
+     * 用于双击事件处理。
+     */
     private GestureDetector mGestureDetector;
+
+    /**
+     * 是否正在处理双击事件
+     */
+    private boolean isDoubleClick = false;
+
+    /**
+     * 最后的多点的数量
+     */
+    private int mLastPointerCount;
+
+    /**
+     * 多点触控时的中心点坐标
+     */
+    private float mCenterX;
+
+    /**
+     * 多点触控时的中心点坐标
+     */
+    private float mCenterY;
 
     public ZoomImageView(Context context) {
         this(context, null);
@@ -57,88 +112,90 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
 
     public ZoomImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
-        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        init(context);
     }
 
-    private boolean isAutoScale = false;
-
-    private void init() {
-        matrix = new Matrix();
+    private void init(Context context) {
+        mMatrix = new Matrix();
         setScaleType(ScaleType.MATRIX);
-        mScaleGestureDetector = new ScaleGestureDetector(getContext(), this);
+        mScaleGestureDetector = new ScaleGestureDetector(context, this);
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
         setOnTouchListener(this);
         mGestureDetector = new GestureDetector(getContext(), new GestureDetector.SimpleOnGestureListener() {
 
             @Override
             public boolean onDoubleTap(MotionEvent e) {
 
-                if(isAutoScale){
+                if (isDoubleClick) {
                     return true;
                 }
-
                 float x = getX();
                 float y = getY();
-
                 if (getScale() < mMidScale) {
-//                    matrix.postScale(mMidScale / getScale(), mMidScale / getScale(), x, y);
-                    postDelayed(new AutoScaleRunnable(mMidScale,x,y),16);
-                    isAutoScale = true;
+                    postDelayed(new AutoScaleRunnable(ZoomImageView.this, mMidScale, x, y), 16);
+                    isDoubleClick = true;
                 } else {
-//                    matrix.postScale(mInitScale / getScale(), mInitScale / getScale(), x, y);
-                    postDelayed(new AutoScaleRunnable(mInitScale,x,y),16);
-                    isAutoScale = true;
+                    postDelayed(new AutoScaleRunnable(ZoomImageView.this, mInitScale, x, y), 16);
+                    isDoubleClick = true;
                 }
-
-                setImageMatrix(matrix);
+                setImageMatrix(mMatrix);
                 return true;
             }
         });
     }
 
+    /**
+     * 平滑缩放处理，用属性动画应该会好很多
+     */
+    private static class AutoScaleRunnable implements Runnable {
 
-    // 是否可以用属性动画来做
-    private class AutoScaleRunnable implements Runnable {
-        /**
-         * 缩放的目标值
-         */
+        private WeakReference<ZoomImageView> zoomImageViewWeakReference;
+
         private float mTargetScale;
         private float x;
         private float y;
 
-        private final float BIGGIR = 1.07f;
+        private final float BIG = 1.07f;
         private final float SMALL = 0.9f;
 
         private float tmpScale;
 
-        public AutoScaleRunnable(float mTargetScale, float x, float y) {
+        public AutoScaleRunnable(ZoomImageView zoomImageView, float mTargetScale, float x, float y) {
             this.mTargetScale = mTargetScale;
             this.x = x;
             this.y = y;
-            if (getScale() < mTargetScale) {
-                tmpScale = BIGGIR;
+            this.zoomImageViewWeakReference = new WeakReference<>(zoomImageView);
+
+            if (zoomImageViewWeakReference.get() == null) {
+                return;
             }
-            if (getScale() >= mTargetScale) {
+            if (zoomImageViewWeakReference.get().getScale() < mTargetScale) {
+                tmpScale = BIG;
+            }
+            if (zoomImageViewWeakReference.get().getScale() >= mTargetScale) {
                 tmpScale = SMALL;
             }
         }
 
         @Override
         public void run() {
-            matrix.postScale(tmpScale, tmpScale, x, y);
-            checkBorderAndCenter();
-            setImageMatrix(matrix);
+            if (zoomImageViewWeakReference.get() == null) {
+                return;
+            }
+            ZoomImageView zoomImageView = zoomImageViewWeakReference.get();
+            zoomImageView.mMatrix.postScale(tmpScale, tmpScale, x, y);
+            zoomImageView.checkBorderWhenScale();
+            zoomImageView.setImageMatrix(zoomImageView.mMatrix);
 
-            float current = getScale();
+            float current = zoomImageView.getScale();
             if ((tmpScale > 1.0f && current < mTargetScale) || (tmpScale < 1.0f && current > mTargetScale)) {
-                postDelayed(this, 16);
-
+                zoomImageView.postDelayed(this, 16);
             } else {
                 float scale = mTargetScale / current;
-                matrix.postScale(scale, scale, x, y);
-                checkBorderAndCenter();
-                setImageMatrix(matrix);
-                isAutoScale = false;
+                zoomImageView.mMatrix.postScale(scale, scale, x, y);
+                zoomImageView.checkBorderWhenScale();
+                zoomImageView.setImageMatrix(zoomImageView.mMatrix);
+                zoomImageView.isDoubleClick = false;
             }
         }
     }
@@ -157,12 +214,13 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
     }
 
     /**
-     * 布局完成布局之后会调用此方法
+     * 布局完成布局之后会调用此方法，计算出图片的初始缩放值，
+     * 将图片平移缩放在布局中央。
      */
     @Override
     public void onGlobalLayout() {
-        if (!init) {
-            init = true;
+        if (!isInit) {
+            isInit = true;
 
             // 获取控件的宽高
             int viewWidth = getWidth();
@@ -200,16 +258,16 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
             int tranX = viewWidth / 2 - dw / 2;
             int tranY = viewHeight / 2 - dh / 2;
 
-            matrix.postTranslate(tranX, tranY);
-            matrix.postScale(mInitScale, mInitScale, viewWidth / 2, viewHeight / 2);
-            setImageMatrix(matrix);
+            mMatrix.postTranslate(tranX, tranY);
+            mMatrix.postScale(mInitScale, mInitScale, viewWidth / 2, viewHeight / 2);
+            setImageMatrix(mMatrix);
         }
     }
 
     // 获取缩放值
     public float getScale() {
         float[] values = new float[9];
-        matrix.getValues(values);
+        mMatrix.getValues(values);
         return values[Matrix.MSCALE_X];
     }
 
@@ -224,25 +282,19 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
             return true;
         }
 
-        // 控制缩放范围
         if ((scale < mMaxScale && scaleFactor > 1.0f)
                 || (scale > mInitScale && scaleFactor < 1.0f)) {
 
             if (scale * scaleFactor < mInitScale) {
                 scaleFactor = mInitScale / scale;
             }
-
             if (scale * scaleFactor > mMaxScale) {
                 scale = mMaxScale / scale;
             }
-
-            matrix.postScale(scaleFactor, scaleFactor, detector.getFocusX(), detector.getFocusY());
-
-            checkBorderAndCenter();
-            setImageMatrix(matrix);
+            mMatrix.postScale(scaleFactor, scaleFactor, detector.getFocusX(), detector.getFocusY());
+            checkBorderWhenScale();
+            setImageMatrix(mMatrix);
         }
-
-
         return true;
     }
 
@@ -252,7 +304,7 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
      * @return
      */
     private RectF getMatrixRectF() {
-        Matrix getMatrix = matrix;
+        Matrix getMatrix = mMatrix;
         RectF rectF = new RectF();
 
         Drawable drawable = getDrawable();
@@ -261,49 +313,6 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
             getMatrix.mapRect(rectF);
         }
         return rectF;
-    }
-
-
-    /**
-     * 放大缩小边界检测
-     */
-    private void checkBorderAndCenter() {
-        RectF rectF = getMatrixRectF();
-        float deltaX = 0;
-        float deltaY = 0;
-
-        int width = getWidth();
-        int height = getHeight();
-        if (rectF.width() >= width) {
-            if (rectF.left > 0) {
-                deltaX = -rectF.left;
-            }
-
-            if (rectF.right < width) {
-                deltaX = width - rectF.right;
-            }
-        }
-
-        if (rectF.height() >= height) {
-            if (rectF.top >= height) {
-                deltaY = -rectF.top;
-            }
-
-            if (rectF.bottom < height) {
-                deltaY = height - rectF.bottom;
-            }
-        }
-
-        if (rectF.width() < width) {
-            deltaX = width / 2 - rectF.right + rectF.width() / 2;
-        }
-
-        if (rectF.height() < height) {
-            deltaY = height / 2 - rectF.bottom + rectF.height() / 2;
-        }
-
-        matrix.postTranslate(deltaX, deltaY);
-
     }
 
     @Override
@@ -316,48 +325,37 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
 
     }
 
-    // 自由移动的
-    // 自由移动的位置
-    private int mLastPointerCount;
-
-    private float mLastX;
-
-    private float mLastY;
-
     @Override
     public boolean onTouch(View v, MotionEvent event) {
 
+        float x = 0;
+        float y = 0;
         // 先双击判断
         if (mGestureDetector.onTouchEvent(event)) {
             return true;
         }
-
         mScaleGestureDetector.onTouchEvent(event);
 
-
-        float x = 0;
-        float y = 0;
-
+        // 计算出多点触控时，所有点的中心点位置
         int pointCount = event.getPointerCount();
         for (int i = 0; i < pointCount; i++) {
             x += event.getX(i);
             y += event.getY(i);
         }
-
         x /= pointCount;
         y /= pointCount;
 
         if (mLastPointerCount != pointCount) {
             isCanMove = false;
-            mLastX = x;
-            mLastY = y;
+            mCenterX = x;
+            mCenterY = y;
         }
         mLastPointerCount = pointCount;
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_MOVE:
-                float dx = x - mLastX;
-                float dy = y - mLastY;
+                float dx = x - mCenterX;
+                float dy = y - mCenterY;
                 if (!isCanMove) {
                     isCanMove = isMoveAction(dx, dy);
                 }
@@ -369,19 +367,17 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
                             isCheckLeftAndRight = false;
                             dx = 0;
                         }
-
                         if (rectF.height() < getHeight()) {
                             isCheckTopAndBottom = false;
                             dy = 0;
                         }
-
-                        matrix.postTranslate(dx, dy);
+                        mMatrix.postTranslate(dx, dy);
                         checkBorderWhenTranslate();
-                        setImageMatrix(matrix);
+                        setImageMatrix(mMatrix);
                     }
                 }
-                mLastX = x;
-                mLastY = y;
+                mCenterX = x;
+                mCenterY = y;
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
@@ -419,10 +415,57 @@ public class ZoomImageView extends android.support.v7.widget.AppCompatImageView
         if (rectF.right < width && isCheckLeftAndRight) {
             deltaX = width - rectF.right;
         }
-        matrix.postTranslate(deltaX, deltaY);
+        mMatrix.postTranslate(deltaX, deltaY);
+    }
+
+    /**
+     * 放大缩小边界检测
+     */
+    private void checkBorderWhenScale() {
+        RectF rectF = getMatrixRectF();
+        float deltaX = 0;
+        float deltaY = 0;
+
+        int width = getWidth();
+        int height = getHeight();
+        if (rectF.width() >= width) {
+            if (rectF.left > 0) {
+                deltaX = -rectF.left;
+            }
+            if (rectF.right < width) {
+                deltaX = width - rectF.right;
+            }
+        }
+
+        if (rectF.height() >= height) {
+            if (rectF.top >= height) {
+                deltaY = -rectF.top;
+            }
+
+            if (rectF.bottom < height) {
+                deltaY = height - rectF.bottom;
+            }
+        }
+
+        if (rectF.width() < width) {
+            deltaX = width / 2 - rectF.right + rectF.width() / 2;
+        }
+
+        if (rectF.height() < height) {
+            deltaY = height / 2 - rectF.bottom + rectF.height() / 2;
+        }
+
+        mMatrix.postTranslate(deltaX, deltaY);
     }
 
 
+    /**
+     * 判断移动的距离是否算是移动
+     *
+     * @param x
+     * @param y
+     * @return
+     */
     private boolean isMoveAction(float x, float y) {
         return Math.sqrt(x * x + y * y) > mTouchSlop;
     }
