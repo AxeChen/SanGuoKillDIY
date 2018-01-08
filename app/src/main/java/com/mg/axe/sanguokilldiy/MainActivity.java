fd868d31c892ccf98f33fd13bb2f7d7a1a00f1e7
axeChen <1137366723@qq.com>
2018-01-08 15:47:38 +0800

    替换资源文件

--- app/src/main/java/com/mg/axe/sanguokilldiy/MainActivity.java
+++ app/src/main/java/com/mg/axe/sanguokilldiy/MainActivity.java
@@ -6,10 +6,6 @@ import android.support.v7.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
-    static {
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
